9b6ecb59ec81fafaea713998056c02b0ad3bfdc4
Alexander Kolbasov <akolb@cloudera.com>
2020-05-26 21:49:49 -0700

    Use tempdir directory for scratch files.

--- tooltests/tool_test.py
+++ tooltests/tool_test.py
@@ -13,29 +13,25 @@ test_cases = {
         "name": "simple_case_1",
         "cmd": path.join(TEST_DIR, "copier.py"),
         "input": path.join(FIXTURE_DIR, "input.txt"),
-        "output": "/tmp/foo.out",
+        "output": "foo.out",
         "expected": path.join(FIXTURE_DIR, "input_expected.txt"),
         "ok": True,
     },
 }
 
 
-def cleanup(test_name):
-    if test_name not in test_cases: return
-    test_case = test_cases[test_name]
-    scratch_file = test_case["output"]
-    if not path.exists(scratch_file): return
-    remove(scratch_file)
+def cleanup(file):
+    remove(file)
 
 
-def test_case_1():
+def test_case_1(tmpdir):
     name = "simple_case_1"
     test_case = test_cases[name]
     assert test_case is not None
-    rc = system([test_case["cmd"], test_case["input"],
-                 test_case["output"]])
+    output = path.join(tmpdir, test_case["output"])
+    rc = system([test_case["cmd"], test_case["input"], output])
     try:
         assert rc == test_case["ok"]
-        assert cmp(test_case["output"], test_case["expected"]) == True
+        assert cmp(output, test_case["expected"]) == True
     finally:
-        cleanup(name)
+        cleanup(output)
